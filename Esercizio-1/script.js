5749047e682df522305b983c83cd41aa46c30bd0
Nicola <nicolamelito.11@gmail.com>
2020-10-27 17:08:12 +0100

    modifica sul posizionamento della funzione

--- Esercizio-1/script.js
+++ Esercizio-1/script.js
@@ -1,11 +1,14 @@
 var utenteWord = prompt('inserisci una parola');
 var arrInverse = stringaInversa(utenteWord);
-function stringaInversa(word) {
-return word.toLowerCase().split("").reverse().join("");
-}
+
 
   if (utenteWord === arrInverse) {
     document.getElementById('parola').innerHTML = utenteWord + '  è una palindroma';
   } else {
      document.getElementById('parola').innerHTML = utenteWord + ' non è una palindroma';
+  }
+
+
+  function stringaInversa(word) {
+  return word.toLowerCase().split("").reverse().join("");
   }
