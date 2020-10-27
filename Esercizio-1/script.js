f88b555d02effbe8b562fb10495296c73ada6def
Nicola <nicolamelito.11@gmail.com>
2020-10-27 16:43:05 +0100

    modifica sul message

--- Esercizio-1/script.js
+++ Esercizio-1/script.js
@@ -1,13 +1,11 @@
 var utenteWord = prompt('inserisci una parola');
 var arrInverse = utenteWord.split("").reverse().join("");
-var message = false;
 function stringaInversa(utenteWord) {
 return stringaInversa.toLowerCase().split("").reverse().join("");
 }
 
   if (utenteWord === stringaInversa) {
-      message = false;
-      document.getElementById('parola').innerHTML = message + ' non è una palindroma';
+    document.getElementById('parola').innerHTML = utenteWord + '  è una palindroma';
   } else {
-     document.getElementById('parola').innerHTML = message + ' è una palindroma';
+     document.getElementById('parola').innerHTML = utenteWord + ' non è una palindroma';
   }
